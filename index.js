1d26ffa7fd25d409670cf967a3ba4fc6e7de87d9
+dorcus <+wayumulu58@gmail.com>
2022-06-15 23:16:50 +0300

    assignment commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+const formData = {
+  name: "Dorcas",
+  email: "wayumulu58@gmail.com",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  });
